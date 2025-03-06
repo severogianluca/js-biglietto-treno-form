366e34a44764656ae29a3b6d094904b2b44eadb7
severogianluca <gianlucasevero300@gmail.com>
2025-03-06 04:24:51 +0100

    Feat: create condizione per assegnazione  classe e carrozza

--- app.js
+++ app.js
@@ -38,13 +38,35 @@ formElement.addEventListener('submit', function (event) {
 
 
     // CREAZIONE DEL BIGLIETTO ASSOCIATO AI VALORI DEL SUBMIT
+
+    // **Logica per assegnare la classe dell'offerta in base ai km e assegnazione della carrozza**
+    let offertaTreno;
+    let numeroCarrozza;
+    if (km < 100) {
+        offertaTreno = "Classe Standard";
+        numeroCarrozza = Math.ceil(Math.random() * 4);
+    } else if (km >= 100 && km <= 200) {
+        offertaTreno = "Classe Superior";
+        numeroCarrozza = Math.ceil(Math.random() * 3) + 4;
+    } else {
+        offertaTreno = "Super Classe";
+        numeroCarrozza = Math.ceil(Math.random() * 3) + 7;
+    }
+
+
+
     let nameBiglietto = document.getElementById('name-biglietto');
+    let offertaBiglietto = document.getElementById('offerta-biglietto')
+    let carrozzaBiglietto = document.getElementById('carrozza-biglietto');
+    let codiceBiglietto = document.getElementById('codice-biglietto');
     let prezzoBiglietto = document.getElementById('prezzo-biglietto');
-    let etaBiglietto = document.getElementById('eta-biglietto');
 
     nameBiglietto.innerHTML = dati;
+    offertaBiglietto.innerHTML = offertaTreno;
+    carrozzaBiglietto.innerHTML = numeroCarrozza;
+    codiceBiglietto.innerHTML = codiceCasuale;
     prezzoBiglietto.innerHTML = prezzo;
-    etaBiglietto.innerHTML = eta;
+
 
     let elementBiglietto = document.getElementById('biglietto');
     elementBiglietto.style.display = 'block';
