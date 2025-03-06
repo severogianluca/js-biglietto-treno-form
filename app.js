99b164c5af74efe549bee17d402abebd91adfa2f
severogianluca <gianlucasevero300@gmail.com>
2025-03-06 03:43:35 +0100

    Feat: create biglietto

--- app.js
+++ app.js
@@ -12,34 +12,46 @@ formElement.addEventListener('submit', function (event) {
     let dati = elementDati.value;
     let km = parseInt(elementKm.value);
     let eta = elementEta.value;
-    
-    let prezzo = km *  (0.21);
+
+    let prezzo = km * (0.21);
     let sconto;
     console.log(dati)
     console.log(eta);
     console.log(km);
-    
-    
+
+
     // logica per lo sconto
-    if(eta === 'minorenne'){
+    if (eta === 'minorenne') {
         sconto = ((prezzo * 20) / 100);
-    
-    }else if(eta === 'over-65' ){
+
+    } else if (eta === 'over-65') {
         sconto = ((prezzo * 40) / 100);
-        
-    }else{
+
+    } else {
         sconto = 0;
     }
-    
+
     prezzo = prezzo - sconto;
     prezzo = prezzo.toFixed(2);
     console.log(prezzo);
     console.log(`il prezzo del biglietto è di: ${prezzo} €`)
-    
-    creaBiglietto();
-});
 
-function creaBiglietto(){
+
+    // CREAZIONE DEL BIGLIETTO ASSOCIATO AI VALORI DEL SUBMIT
+    let nameBiglietto = document.getElementById('name-biglietto');
+    let prezzoBiglietto = document.getElementById('prezzo-biglietto');
+    let etaBiglietto = document.getElementById('eta-biglietto');
+
+    nameBiglietto.innerHTML = dati;
+    prezzoBiglietto.innerHTML = prezzo;
+    etaBiglietto.innerHTML = eta;
+
     let elementBiglietto = document.getElementById('biglietto');
     elementBiglietto.style.display = 'block';
-}
+
+});
+
+// function creaBiglietto(){
+//     let elementBiglietto = document.getElementById('biglietto');
+//     elementBiglietto.style.display = 'block';
+// }
